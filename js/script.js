9dcc9594c8f628e72ec0bb7db0889f6d296446f1
somaiya shahin <somaiyashahin484@gmail.com>
2023-02-12 02:49:34 +0600

    navigate to another page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 document.getElementById('btn-submit').addEventListener('click', function(){
     //get email
-    const email = document.getElementById('enter-email');
-    console.log('slfjise')
+    const emailField = document.getElementById('enter-email');
+    const email = emailField.value;
+
+    //get password
+    const passwordField = document.getElementById("enter-password");
+    const password = passwordField.value;
+
+    if(email === 'somaiya@gmail.com' && password === 'somaiya'){
+        window.location.href = 'bank.html';
+    }
 })
