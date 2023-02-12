a30f51a495fdeb5734a6735049b4d1bf0b241126
somaiya shahin <somaiyashahin484@gmail.com>
2023-02-12 17:37:05 +0600

    remove previous code comment

--- js/deposiit.js
+++ js/deposiit.js
@@ -1,28 +1,3 @@
-// document.getElementById('btn-deposit').addEventListener('click', function(){
-//     const depositField = document.getElementById('deposit-field');
-//     // const newDepositAmount = depositField.value; ata nibo na. kanona ata string akare acha. atake parsefloat kore number e nite hobe. 
-//     const newDepositAmountString = depositField.value;
-//     const newDepositAmount = parseFloat(newDepositAmountString);
-
-//     const depositTotalElement = document.getElementById('deposit-total');
-//     // const previousDepositTotal = depositTotalElement.innerText;akoi vabe previous keo number e nite hobe string theke.
-
-//     const previousDepositTotalElementString = depositTotalElement.innerText;
-//     const previousDepositTotal = parseFloat(previousDepositTotalElementString);
-//     const currentDepositTotal = previousDepositTotal + newDepositAmount;
-
-//     depositTotalElement.innerText = currentDepositTotal;
-    
-//     const balanceTotalElement = document.getElementById('balance-total');
-//     const previousBalanceTotalString = balanceTotalElement.innerText;
-//     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
-//     const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
-//     balanceTotalElement.innerText = currentBalanceTotal; 
-
-//     depositField.value = '';
-// })
-
-
 document.getElementById('btn-deposit').addEventListener('click', function(){
     const depositField = document.getElementById('deposit-field');
     const newDepositAmountString = depositField.value;
@@ -37,7 +12,7 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const currentDepositTotal = depositTotalElement + newDepositAmount;
     depositTotal.innerText = currentDepositTotal;
 
-    // add new deposit to previous deposit
+    // add new deposit with previous deposit
     const previousBalanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = previousBalanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
