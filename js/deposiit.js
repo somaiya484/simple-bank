c197cf18ad3fece245343c802db92004c94dbdc6
somaiya shahin <somaiyashahin484@gmail.com>
2023-02-12 15:59:55 +0600

    final deposit section

--- js/deposiit.js
+++ js/deposiit.js
@@ -1,12 +1,23 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
-
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    // const newDepositAmount = depositField.value; ata nibo na. kanona ata string akare acha. atake parsefloat kore number e nite hobe. 
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
 
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    // const previousDepositTotal = depositTotalElement.innerText;akoi vabe previous keo number e nite hobe string theke.
 
+    const previousDepositTotalElementString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalElementString);
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
 
+    depositTotalElement.innerText = currentDepositTotal;
+    
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanceTotal; 
 
+    depositField.value = '';
 })
