768fefad3d06cb4b59e61b3e9ecc74e3e63a4418
somaiya shahin <somaiyashahin484@gmail.com>
2023-02-12 17:15:08 +0600

    navigate to another page done

--- js/script.js
+++ js/script.js
@@ -21,5 +21,17 @@
 
 
 document.getElementById('btn-submit').addEventListener('click', function(){
-    
+    const userEmail = document.getElementById('enter-email');
+    const email = userEmail.value;
+    const userPassword = document.getElementById('enter-password');
+    const password = userPassword.value;
+
+    // check validation and navigate to another page
+    if(email === 'somaiya@gmail.com' && password === '123'){
+        window.location.href = 'bank.html'
+    }else{
+        alert('Your email or password is wrong. Try tto give valid user or password')
+    }
+    userEmail.value = '';
+    userPassword.value = '';
 })
