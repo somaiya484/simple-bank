b39262dbbd2921199ef3ed9f55e4754596aad4d5
somaiya shahin <somaiyashahin484@gmail.com>
2023-02-12 16:37:59 +0600

    validation added

--- js/withdraw.js
+++ js/withdraw.js
@@ -7,20 +7,22 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const previousWithdrawTotalString = WithdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    WithdrawTotalElement.innerText = currentWithdrawTotal;
-
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+    
+    withdrawFiled.value = '';
+    
     if(newWithdrawAmount > previousBalanceTotal){
-        alert('bap er bank e ato taka nai')
+        alert('bap er bank e ato taka nai');
+        return;
     }
 
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    WithdrawTotalElement.innerText = currentWithdrawTotal;
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
 
     balanceTotalElement.innerText = newBalanceTotal
 
-    withdrawFiled.value = '';
 })
