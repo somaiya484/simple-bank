812aadf79a7b85cfd47beee6c5e282d38bfffc16
somaiya shahin <somaiyashahin484@gmail.com>
2023-02-12 15:26:56 +0600

    conneted with deposite amount

--- js/deposiit.js
+++ js/deposiit.js
@@ -1,2 +1,12 @@
-document.getElementById('btn-deposit');
-console.log('hello')
+document.getElementById('btn-deposit').addEventListener('click', function(){
+
+    const depositField = document.getElementById('deposit-field');
+    const depositAmount = depositField.value;
+
+    const depositTotalElement = document.getElementById('deposit-total');
+    const depositTotal = depositTotalElement.innerText;
+    console.log(depositTotal)
+
+
+
+})
